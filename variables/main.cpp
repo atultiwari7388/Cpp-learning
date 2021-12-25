978fe047842d65f7093a707e72cb372d5c3afef1
Atul Tiwari <tiwariatul9526@gmail.com>
2021-12-25 17:37:57 +0530

    variables

---
+++ variables/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  string charName = "John";
+  int charAge = 20;
+
+  cout<< "There once was a man named "<< charName << endl;
+  cout<< "He was "  << charAge << " year old" << endl;
+  charName = "John Cena";
+  cout<< "He liked the name "<< charName << endl;
+  cout<< "But did not like being "<< charAge  << endl;
+
+}
