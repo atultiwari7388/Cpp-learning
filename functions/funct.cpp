21b38310e876329fb526d0a320b547c41dddcf0d
Atul Tiwari <tiwariatul9526@gmail.com>
2021-12-25 20:24:16 +0530

    functions

---
+++ functions/funct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHello(string name, int age){
+  cout << "Hello "  << name << " are you a Programmer ? " << " you are " << age << " years old" << endl;
+}
+
+int main(){
+  sayHello("John", 21);
+  return 0;
+
+}
+
+
