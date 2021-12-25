5003d564111eedc15d7bfb302e1bf1a3e490618d
Atul Tiwari <tiwariatul9526@gmail.com>
2021-12-25 18:48:10 +0530

    getting user input

---
+++ Numbers/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+ int age;
+ cout << "Enter your age:\n ";
+
+ string name;
+  cout << "Enter your name:\n ";
+  getline(cin, name);
+  cout << "Hello " << name << "! You are " << age << " years old." << endl;
+
+
+
+  return 0;
+}
