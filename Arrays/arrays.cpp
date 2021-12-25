711f0c0598943bb9c7033547f6477fac5df11638
Atul Tiwari <tiwariatul9526@gmail.com>
2021-12-25 19:38:40 +0530

    arrays

---
+++ Arrays/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int luckyNumbers[] = {4, 8, 15, 16, 23};
+  luckyNumbers[0]= 18;
+  cout << luckyNumbers[0] << endl;
+
+  return 0;
+}
