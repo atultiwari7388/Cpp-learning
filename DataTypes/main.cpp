44927c0c56d65c7befb19d7f4615679571fc7240
Atul Tiwari <tiwariatul9526@gmail.com>
2021-12-25 17:49:01 +0530

    data types

---
+++ DataTypes/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  char grade = 'A';
+  string phrase =  "Hello World";
+  int number = 10;
+  float num = 10.5;
+  double gpa = 10.5;
+  bool isMale = true;
+ 
+  return 0;
+
+}
